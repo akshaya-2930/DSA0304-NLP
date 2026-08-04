8443b75f6a03d0c98d09310ea30adedfc6fa735a
akshaya-2930 <akkiakshaya142@gmail.com>
2026-08-04 21:50:04 +0530

    Delete experiment22.py

--- experiment22.py
+++
@@ -1,9 +0,0 @@
-text = "Akshaya is a good student. She studies Python every day."
-
-print("Original Text:")
-print(text)
-
-resolved = text.replace("She", "Akshaya")
-
-print("\nAfter Reference Resolution:")
-print(resolved)
